f2459d4804d6657d37ae080b8f14c5af0fa8b8fb
Diego Rossinelli <diegor@inf.ethz.ch>
2013-03-19 15:04:53 +0100

    including test clouds at node layer

---
+++ CubismApps/MPCFnode/source/Test_Cloud.cpp
@@ -0,0 +1,244 @@
+/*
+ *  Test_Cloud.cpp
+ *  MPCFnode
+ *
+ *  Created by Babak Hejazialhosseini  on 2/24/13.
+ *  Copyright 2011 ETH Zurich. All rights reserved.
+ *
+ */
+#include <limits>
+#include <sstream>
+
+#ifdef _USE_NUMA_
+#include <numa.h>
+#include <omp.h>
+#endif
+
+#include <Profiler.h>
+
+#include "Test_Cloud.h"
+#include "Tests.h"
+
+void Test_Cloud::_ic(FluidGrid& grid)
+{
+    cout << "Not supposed to call _ic in Cloud\n" ;
+    cout << "Should call _my_ic instead\n" ;
+    abort();
+}
+
+FluidElement operator * (Real a, FluidElement gp)
+{
+    FluidElement out;
+ 
+    out.rho = gp.rho * a;
+    out.u = gp.u * a;
+    out.v = gp.v * a;
+    out.w = gp.w * a;
+    out.energy = gp.energy * a;
+    out.G = gp.G * a;
+    out.P = gp.P * a;
+    
+    return out;
+}
+
+FluidElement operator + (FluidElement gpa, FluidElement gpb)
+{
+    FluidElement out;
+    
+    out.rho = gpa.rho+gpb.rho;
+    out.u = gpa.u+gpb.u;
+    out.v = gpa.v+gpb.v;
+    out.w = gpa.w+gpb.w;
+    out.energy = gpa.energy+gpb.energy;
+    out.G = gpa.G+gpb.G;
+    out.P = gpa.P+gpb.P;
+    
+    return out;
+}
+
+template<typename T>
+T get_ic(const Real p[3], const vector< shape * > v_shapes)
+{
+    T out;
+    
+    const double G1 = Simulation_Environment::GAMMA1-1;
+    const double G2 = Simulation_Environment::GAMMA2-1;
+    const double F1 = Simulation_Environment::GAMMA1*Simulation_Environment::PC1;
+    const double F2 = Simulation_Environment::GAMMA2*Simulation_Environment::PC2;
+    
+    const double bubble = eval(v_shapes, p);
+    
+    const Real pre_shock[3] = {1000,0,100};
+    
+    out.rho      = 1.0*bubble+pre_shock[0]*(1-bubble);
+    out.u        = 0;
+    out.v        = 0;
+    out.w        = 0;
+    
+    const double pressure  = 0.0234*bubble+pre_shock[2]*(1-bubble);
+    
+    const double mix_gamma = 1 + (G2*G1)/(G1*bubble+G2*(1-bubble));
+    const double mix_pinf  = (mix_gamma-1)/mix_gamma * (F1/G1*(1-bubble) + F2/G2*bubble);
+    out.G  = 1./(mix_gamma-1);
+    out.P = mix_gamma*mix_pinf/(mix_gamma-1);
+    out.energy   = pressure*out.G + out.P;
+    
+    return out;    
+}
+
+template<typename T>
+T integral(const Real p[3], float h, const vector< shape * > v_shapes) // h should be cubism h/2
+{
+	T samples[3][3][3];
+	T zintegrals[3][3];
+	T yzintegrals[3];
+	
+	const Real x0[3] = {p[0] -h, p[1] - h, p[2] - h};
+	
+	for(int iz=0; iz<3; iz++)
+		for(int iy=0; iy<3; iy++)
+			for(int ix=0; ix<3; ix++)
+            {
+                const Real mypos[3] = {x0[0]+ix*h, x0[1]+iy*h, x0[2]+iz*h};
+				samples[iz][iy][ix] = get_ic<T>(mypos, v_shapes);
+            }
+	
+	for(int iy=0; iy<3; iy++)
+		for(int ix=0; ix<3; ix++)
+			zintegrals[iy][ix] = (1/6.) * samples[0][iy][ix]+(2./3) * samples[1][iy][ix]+(1/6.)* samples[2][iy][ix];
+	
+	for(int ix=0; ix<3; ix++)
+		yzintegrals[ix] = (1./6)*zintegrals[0][ix] + (2./3)*zintegrals[1][ix]+(1./6)*zintegrals[2][ix];
+	
+	return (1./6) * yzintegrals[0]+(2./3) * yzintegrals[1]+(1./6)* yzintegrals[2];
+}
+
+void Test_Cloud::_my_ic_quad(FluidGrid& grid, const vector< shape * > v_shapes)
+{
+	if (VERBOSITY > 0)
+		cout << "Cloud Initial condition..." ;
+    
+	vector<BlockInfo> vInfo = grid.getBlocksInfo();
+    
+    const double h = vInfo.front().h_gridpoint;
+    
+#pragma omp parallel
+	{
+#ifdef _USE_NUMA_
+		const int cores_per_node = numa_num_configured_cpus() / numa_num_configured_nodes();
+		const int mynode = omp_get_thread_num() / cores_per_node;
+		numa_run_on_node(mynode);
+#endif
+        
+#pragma omp for
+		for(int i=0; i<(int)vInfo.size(); i++)
+		{
+            BlockInfo info = vInfo[i];
+            FluidBlock& b = *(FluidBlock*)info.ptrBlock;
+            
+            for(int iz=0; iz<FluidBlock::sizeZ; iz++)
+                for(int iy=0; iy<FluidBlock::sizeY; iy++)
+                    for(int ix=0; ix<FluidBlock::sizeX; ix++)
+                    {
+                        Real p[3], post_shock[3];
+                        info.pos(p, ix, iy, iz);
+                        
+                        b(ix,iy,iz) = integral<FluidElement>(p,0.5*h,v_shapes);
+                    }
+        }
+	}
+	
+	if (VERBOSITY > 0)
+		cout << "done." << endl;
+}
+
+void Test_Cloud::_my_ic(FluidGrid& grid, const vector< shape * > v_shapes)
+{
+	if (VERBOSITY > 0)
+		cout << "Cloud Initial condition..." ;
+    
+	vector<BlockInfo> vInfo = grid.getBlocksInfo();
+    
+    const double G1 = Simulation_Environment::GAMMA1-1;
+    const double G2 = Simulation_Environment::GAMMA2-1;
+    const double F1 = Simulation_Environment::GAMMA1*Simulation_Environment::PC1;
+    const double F2 = Simulation_Environment::GAMMA2*Simulation_Environment::PC2;
+    
+    const double h = vInfo.front().h_gridpoint;
+
+#pragma omp parallel
+	{
+#ifdef _USE_NUMA_
+		const int cores_per_node = numa_num_configured_cpus() / numa_num_configured_nodes();
+		const int mynode = omp_get_thread_num() / cores_per_node;
+		numa_run_on_node(mynode);
+#endif
+        
+#pragma omp for
+		for(int i=0; i<(int)vInfo.size(); i++)
+		{
+            BlockInfo info = vInfo[i];
+            FluidBlock& b = *(FluidBlock*)info.ptrBlock;
+            
+            for(int iz=0; iz<FluidBlock::sizeZ; iz++)
+                for(int iy=0; iy<FluidBlock::sizeY; iy++)
+                    for(int ix=0; ix<FluidBlock::sizeX; ix++)
+                    {
+                        Real p[3], post_shock[3];
+                        info.pos(p, ix, iy, iz);
+                        
+                        const double bubble = eval(v_shapes, p);
+                        
+                        const Real pre_shock[3] = {1000,0,100};
+                        
+                        b(ix, iy, iz).rho      = 1.0*bubble+pre_shock[0]*(1-bubble);
+                        b(ix, iy, iz).u        = 0;
+                        b(ix, iy, iz).v        = 0;
+                        b(ix, iy, iz).w        = 0;
+                        
+                        const double pressure  = 0.0234*bubble+pre_shock[2]*(1-bubble);
+                        
+                        SETUP_MARKERS_IC                       
+			  }
+        }
+	}
+	
+	if (VERBOSITY > 0)
+		cout << "done." << endl;
+}
+
+void Test_Cloud::setup()
+{
+  printf("////////////////////////////////////////////////////////////\n");
+  printf("////////////             TEST CLOUD          ///////////////\n");
+  printf("////////////////////////////////////////////////////////////\n");
+    
+  _setup_constants();
+    
+  parser.mute();
+    
+  if (parser("-morton").asBool(0))
+    grid = new GridMorton<FluidGrid>(BPDX, BPDY, BPDZ);
+  else
+    grid = new FluidGrid(BPDX, BPDY, BPDZ);
+    
+  assert(grid != NULL);
+    
+  stepper = new FlowStep_LSRK3(*grid, CFL, Simulation_Environment::GAMMA1, Simulation_Environment::GAMMA2, parser, VERBOSITY, &profiler, Simulation_Environment::PC1, Simulation_Environment::PC2, bAWK);
+    
+  if(bRESTART)
+    {
+      _restart();
+      _dump("restartedcondition.vti");
+    }
+  else
+  {
+      bRestartedSeed = parser("-seed").asBool(0);
+      
+      Seed my_seed(CloudData::seed_s, CloudData::seed_e, CloudData::n_shapes);
+      my_seed.make_shapes(bRestartedSeed);
+      
+      //_my_ic(*grid, my_seed.get_vshapes());
+      _my_ic_quad(*grid, my_seed.get_vshapes());
+  }
+}
---
+++ CubismApps/MPCFnode/source/Test_Cloud.h
@@ -0,0 +1,273 @@
+/*
+ *  Test_Cloud.h
+ *  MPCFnode
+ *
+ *  Created by Babak Hejazialhosseini  on 2/24/13.
+ *  Copyright 2012 ETH Zurich. All rights reserved.
+ *
+ */
+#pragma once
+
+#include "Test_SIC.h"
+#include "Types.h"
+#include <fstream>
+
+namespace CloudData
+{
+    const Real seed_s[3] = {0.2, 0.2, 0.15};
+    const Real seed_e[3] = {0.8, 0.8, 0.50};
+    const int n_shapes = 30;
+};
+
+//base class is a sphere
+class shape
+{
+    Real center[3], radius;
+    Real bbox_s[3], bbox_e[3];
+    Real min_rad, max_rad;
+    
+public:
+    shape(const Real _center[3], const Real _radius): radius(_radius)
+    {
+        for(int i=0; i<3; ++i)
+        {
+            center[i] = _center[i];
+            bbox_s[i] = _center[i]-_radius-2.0*Simulation_Environment::EPSILON;
+            bbox_e[i] = _center[i]+_radius+2.0*Simulation_Environment::EPSILON;
+        }
+        
+        min_rad = 0.025;
+        max_rad = 0.15;
+    }
+    
+    void get_bbox(Real s[3], Real e[3]) const
+    {
+        for(int i=0; i<3; ++i)
+        {
+            s[i] = bbox_s[i];
+            e[i] = bbox_e[i];
+        }
+    }
+    
+    bool inside_my_box(const Real pos[3]) const
+    {
+        const bool bXin = pos[0]>bbox_s[0] && pos[0]<bbox_e[0];
+        const bool bYin = pos[1]>bbox_s[1] && pos[1]<bbox_e[1];
+        const bool bZin = pos[2]>bbox_s[2] && pos[2]<bbox_e[2];
+        
+        return bXin && bYin && bZin;
+    }
+    
+    void get_center(Real c[3]) const
+    {
+        for(int i=0; i<3; ++i)
+            c[i] = center[i];
+    }
+    
+    Real get_rad() const
+    {
+        return radius;
+    }
+    
+    Real get_min_rad() const
+    {
+        return min_rad;
+    }
+    
+    Real get_max_rad() const
+    {
+        return max_rad;
+    }
+    
+    Real eval(const Real pos[3]) const
+    {
+        return sqrt(pow(pos[0]-center[0],2)+pow(pos[1]-center[1],2)+pow(pos[2]-center[2],2))-radius;
+    }
+    
+    //Every other derived shape should implement this method.
+    bool rejection_check(shape * this_shape, const Real start[3], const Real end[3]) const
+    {
+        Real s[3], e[3];
+        this->get_bbox(s,e);
+        
+        const bool bOut = s[0]<start[0] || s[1]<start[1] || s[2]<start[2] ||
+        e[0]>end[0] || e[1]>end[1] || e[2]>end[2];
+        
+        const bool bRadOut = this->get_rad()<this->get_min_rad() || this->get_rad()>this->get_max_rad();
+        
+        if (bOut || bRadOut)
+            return true;
+        
+        if(this!=this_shape)
+        {
+            Real this_s[3], this_e[3];
+            
+            this_shape->get_bbox(this_s,this_e);
+            
+            const Real overlap_start[3] =
+            {
+                max(this_s[0], s[0]),
+                max(this_s[1], s[1]),
+                max(this_s[2], s[2])
+            };
+            
+            const Real overlap_end[3] =
+            {
+                min(this_e[0], e[0]),
+                min(this_e[1], e[1]),
+                min(this_e[2], e[2])
+            };
+            
+            const bool bOverlap = overlap_end[0] > overlap_start[0] && overlap_end[1] > overlap_start[1] && overlap_end[2] > overlap_start[2];
+            
+            if (bOverlap)
+                return true;
+        }
+        
+        return false;
+    }
+};
+
+class Seed
+{
+    Real start[3], end[3];
+    int n_shapes;
+    vector<shape *> v_shapes;
+    
+public:
+    Seed(const Real _start[3], const Real _end[3], const int _n_shapes): n_shapes(_n_shapes)
+    {
+        start[0] = _start[0];
+        start[1] = _start[1];
+        start[2] = _start[2];
+        
+        end[0] = _end[0];
+        end[1] = _end[1];
+        end[2] = _end[2];
+    }
+    
+    void make_shapes(const bool bRestartedSeed=false)
+    {
+        bool bFull = false;
+        
+        unsigned int restarted_seed;
+        
+        //read seed if needed
+        if (bRestartedSeed)
+        {
+            ifstream f_read("seed.dat");
+            if(f_read)
+            {
+                cout << "seed file is there" << endl;
+                f_read >> restarted_seed;
+                f_read.close();
+            }
+            else
+            {
+                cout << "seed file not there...aborting" << endl;
+                abort();
+            }
+        }
+        
+        const unsigned int seed = bRestartedSeed? restarted_seed : time(0);
+        
+        //save seed anyway
+        {
+            ofstream f_save("seed.dat");
+            f_save<<seed;
+            f_save.close();
+        }
+        
+        srand48(seed);
+        
+        while(!bFull)
+        {
+            const Real x_c = (Real)drand48();
+            const Real y_c = (Real)drand48();
+            const Real z_c = (Real)drand48();
+            const Real cur_cen[3] = {x_c, y_c, z_c};
+            const Real cur_rad = (Real)drand48();
+            
+            shape * cur_shape = new shape(cur_cen, cur_rad);
+            
+            if (reject_check(cur_shape))
+            {
+                delete cur_shape;
+                continue;
+            }
+            
+            v_shapes.push_back(cur_shape);
+            
+            printf("size is %ld out of %d\n", v_shapes.size(), n_shapes);
+            
+            bFull = v_shapes.size()==n_shapes;
+        }
+        
+        //We are even more paranoic so we save the bubble centers and radii
+        //If the first method does not work on different platforms,
+        //a method must be implemented to read the following data.
+        {
+            ofstream f_save("cloud.dat");
+            for(int i=0; i< v_shapes.size(); i++)
+            {
+                Real c[3], rad;
+                rad = v_shapes[i]->get_rad();
+                v_shapes[i]->get_center(c);
+                f_save<<i<< " " << c[0] << " " << c[1] << " " << c[2] << " " << rad << endl;
+            }
+            
+            f_save.close();
+        }
+    }
+    
+    bool reject_check(shape * cur_shape) const
+    {
+        if (v_shapes.size()==0)
+            return cur_shape->rejection_check(cur_shape, start, end);
+        
+        for(int i=0; i<v_shapes.size(); ++i)
+        {
+            shape * this_shape = v_shapes[i];
+            if (cur_shape->rejection_check(this_shape, start, end))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    vector<shape*> get_vshapes() const
+    {
+        return v_shapes;
+    }
+};
+
+inline double eval(const vector<shape*> v_shapes, const Real pos[3])
+{
+    Real d = HUGE_VAL;
+    
+    for( int i=0; i<v_shapes.size(); ++i)
+    {
+        shape * cur_shape = v_shapes[i];
+        d = min(d, cur_shape->eval(pos));
+    }
+    
+    const double alpha = M_PI*min(1., max(0., (d+0.5*Simulation_Environment::EPSILON)/Simulation_Environment::EPSILON));
+    return 0.5+0.5*cos(alpha);
+}
+
+class Test_Cloud: public Test_SIC
+{
+    friend class Test_CloudMPI;
+    
+    bool bRestartedSeed;
+    
+    void _ic(FluidGrid & grid);
+    
+    void _my_ic(FluidGrid& grid, const vector< shape * > v_shapes);
+    void _my_ic_quad(FluidGrid& grid, const vector< shape * > v_shapes);
+    
+public:
+	Test_Cloud(const int argc, const char ** argv): Test_SIC(argc, argv) { }
+    
+    void setup();
+};
