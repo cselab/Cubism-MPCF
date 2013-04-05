203df6babb9ccd9d4ba7bb7bb47c884aeb63294c
Panagiotis Hadjidoukas <phadjido@mavt.ethz.ch>
2013-04-05 18:59:32 +0200

    Update vp2hdf converter

--- CubismApps/tools/vp2hdf/main.cpp
+++ CubismApps/tools/vp2hdf/main.cpp
@@ -13,50 +13,62 @@
 #include <mpi.h>
 #include <hdf5.h>
 #include <omp.h>
+#define _TRANSPOSE_DATA_
+#define _COLLECTIVE_IO_
 
 #include "ArgumentParser.h"
 #include "Reader_WaveletCompression.h"
 
 int main(int argc, const char **argv)
 {
-	ArgumentParser parser(argc, argv);
-	parser.loud();
+	const double init_t0 = omp_get_wtime();
 
-	const string inputfile = parser("-input").asString("none");
-	if (inputfile == "none")
-	{
-		printf("usage: %s -input <filename>\n", argv[0]);
-		exit(1);
-	}
-
-	/* HDF5 APIs definitions */ 	
-	hid_t file_id, dset_id; /* file and dataset identifiers */
-	hid_t filespace, memspace;      /* file and memory dataspace identifiers */
-	hsize_t dims[3]; /* dataset dimensions */
-	hsize_t	count[3];	  /* hyperslab selection parameters */
-	hsize_t	offset[3];
-	id_t	plist_id; /* property list identifier */
-	herr_t	status;
+	/* Initialize MPI */
+	MPI::Init_thread(MPI_THREAD_SERIALIZED);
 
 	/* MPI variables */
 	MPI_Comm comm  = MPI_COMM_WORLD;
 	MPI_Info info  = MPI_INFO_NULL;
-
-	/* Initialize MPI */
-	MPI::Init();
-
 	MPI::Intracomm& mycomm = MPI::COMM_WORLD;
 
 	const int mpi_rank = mycomm.Get_rank();
 	const int mpi_size = mycomm.Get_size();
 
+	const bool isroot = !mpi_rank;
+
+	ArgumentParser argparser(argc, argv);
+
+	if (isroot)
+		argparser.loud();
+	else
+		argparser.mute();
+
+	const string inputfile = argparser("-simdata").asString("none");
+	if (inputfile == "none")
+	{
+		printf("usage: %s -simdata <filename>\n", argv[0]);
+		exit(1);
+	}
+
+	/* HDF5 APIs definitions */
+	hid_t file_id, dset_id; /* file and dataset identifiers */
+	hid_t filespace, memspace;      /* file and memory dataspace identifiers */
+	hsize_t dims[4]; /* dataset dimensions */
+	hsize_t	count[4];	  /* hyperslab selection parameters */
+	hsize_t	offset[4];
+	id_t	plist_id; /* property list identifier */
+	herr_t	status;
+
 #if 1
 	Reader_WaveletCompressionMPI myreader(mycomm, inputfile);
 #else
-	Reader_WaveletCompression myreader;
+	Reader_WaveletCompression myreader(inputfile);
 #endif
-//	myreader.load_file(inputfile);
 	myreader.load_file();
+	const double init_t1 = omp_get_wtime();
+
+
+	const double t0 = omp_get_wtime(); 
 
 	stringstream ss;
 	ss << inputfile << "_np"  << mpi_size ;
@@ -84,10 +96,18 @@ int main(int argc, const char **argv)
 	int NZ = NBZ*_BLOCKSIZE_;
 
 	/* Create the dataspace for the dataset.*/
+#if defined(_TRANSPOSE_DATA_)
 	dims[0] = NX;
 	dims[1] = NY;
 	dims[2] = NZ;
-	filespace = H5Screate_simple(3, dims, NULL);
+	dims[3] = 1;
+#else
+	dims[0] = NZ;
+	dims[1] = NY;
+	dims[2] = NX;
+	dims[3] = 1;
+#endif
+	filespace = H5Screate_simple(4, dims, NULL);
 
 	/* Create the dataset with default properties and close filespace.*/
 	dset_id = H5Dcreate(file_id, "data", H5T_NATIVE_FLOAT, filespace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
@@ -96,57 +116,59 @@ int main(int argc, const char **argv)
 	count[0] = _BLOCKSIZE_;
 	count[1] = _BLOCKSIZE_;
 	count[2] = _BLOCKSIZE_;
-	memspace = H5Screate_simple(3, count, NULL);
+	count[3] = 1;
+	memspace = H5Screate_simple(4, count, NULL);
 
 	/* Create property list for collective dataset write. */
-#if 1
 	plist_id = H5Pcreate(H5P_DATASET_XFER);
-#else
-	hsize_t cdims[3];
-	cdims[0] = _BLOCKSIZE_/2;
-	cdims[1] = _BLOCKSIZE_/2;
-	cdims[2] = _BLOCKSIZE_/2;
-	plist_id = H5Pcreate(H5P_DATASET_XFER);
-	status = H5Pset_chunk (plist_id, 3, cdims);
-	status = H5Pset_deflate (plist_id, 6); 
-#endif
-
+#if defined(_COLLECTIVE_IO_)
 	H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
-	
+#endif	
 	static Real targetdata[_BLOCKSIZE_][_BLOCKSIZE_][_BLOCKSIZE_];
 	static Real storedata[_BLOCKSIZE_*_BLOCKSIZE_*_BLOCKSIZE_];
 
 	const int nblocks = NBX*NBY*NBZ;
 	const int b_end = ((nblocks + (mpi_size - 1))/ mpi_size) * mpi_size; 
 
-	const double t0 = omp_get_wtime(); 
 	for (int b = mpi_rank; b < b_end; b += mpi_size)	
 	{
+#if defined(_TRANSPOSE_DATA_)
 		int x = b / (NBY * NBZ);
 		int y = (b / NBZ) % NBY;
-		int z = b % NBZ;; 
-	
+		int z = b % NBZ;
+#else
+		int z = b / (NBY * NBX);
+		int y = (b / NBX) % NBY;
+		int x = b % NBX;
+#endif
+
 		if (b < nblocks)
 		{
 //			printf("loading block(%d,%d,%d)\n", x, y, z); 
 			myreader.load_block(x, y, z, targetdata);
 	
-#if 1
+#if defined(_TRANSPOSE_DATA_)
 			for (int xb = 0; xb < _BLOCKSIZE_; xb++)
 				for (int yb = 0; yb < _BLOCKSIZE_; yb++)
 					for (int zb = 0; zb < _BLOCKSIZE_; zb++)
 						storedata[xb*_BLOCKSIZE_*_BLOCKSIZE_ + yb*_BLOCKSIZE_ + zb] = targetdata[zb][yb][xb];
-#endif
-		
 			offset[0] = x * count[0];
 			offset[1] = y * count[1];
 			offset[2] = z * count[2];
+			offset[3] = 0;
+#else
+			offset[0] = z * count[0];
+			offset[1] = y * count[1];
+			offset[2] = x * count[2];
+			offset[3] = 0;
+#endif
+		
 
 			/* Select hyperslab in the file */
 			filespace = H5Dget_space(dset_id);
 			H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
 
-#if 1
+#if defined(_TRANSPOSE_DATA_)
 			status = H5Dwrite(dset_id, H5T_NATIVE_FLOAT, memspace, filespace, plist_id, storedata);
 #else
 			status = H5Dwrite(dset_id, H5T_NATIVE_FLOAT, memspace, filespace, plist_id, targetdata);
@@ -167,9 +189,12 @@ int main(int argc, const char **argv)
 
 	if (!mpi_rank)
 	{
-		fprintf(stderr, "Elapsed time = %.3lf seconds\n", t1-t0); fflush(0);
+		fprintf(stderr, "Init time = %.3lf seconds\n", init_t1-init_t0);
+		fprintf(stderr, "Elapsed time = %.3lf seconds\n", t1-t0);
+		fflush(0);
 	}
 	
+
 	/* Close/release resources */
 	H5Sclose(memspace);
 	H5Dclose(dset_id);
@@ -191,17 +216,20 @@ int main(int argc, const char **argv)
 		fprintf(xmf, "     <Topology TopologyType=\"3DCORECTMesh\" Dimensions=\"%d %d %d\"/>\n", (int)dims[0], (int)dims[1], (int)dims[2]);
 		fprintf(xmf, "     <Geometry GeometryType=\"ORIGIN_DXDYDZ\">\n");
 		fprintf(xmf, "       <DataItem Name=\"Origin\" Dimensions=\"3\" NumberType=\"Float\" Precision=\"4\" Format=\"XML\">\n");
-		fprintf(xmf, "%e %e %e\n", 0.,0.,0.);
+		fprintf(xmf, "        %e %e %e\n", 0.,0.,0.);
 		fprintf(xmf, "       </DataItem>\n");
 		fprintf(xmf, "       <DataItem Name=\"Spacing\" Dimensions=\"3\" NumberType=\"Float\" Precision=\"4\" Format=\"XML\">\n");
-		fprintf(xmf, "%e %e %e\n", 1., 1., 1.);
+#if 1
+		fprintf(xmf, "        %e %e %e\n", 1./(Real)max(dims[0],max(dims[1],dims[2])),1./(Real)max(dims[0],max(dims[1],dims[2])),1./(Real)max(dims[0],max(dims[1],dims[2])));
+#else
+		fprintf(xmf, "        %e %e %e\n", grid.getH(), grid.getH(), grid.getH());
+#endif
 		fprintf(xmf, "       </DataItem>\n");
 		fprintf(xmf, "     </Geometry>\n");
 
 		fprintf(xmf, "     <Attribute Name=\"data\" AttributeType=\"%s\" Center=\"Node\">\n", "anything");
 		fprintf(xmf, "       <DataItem Dimensions=\"%d %d %d %d\" NumberType=\"Float\" Precision=\"4\" Format=\"HDF\">\n", (int)dims[0], (int)dims[1], (int)dims[2], 1);
-		
-		fprintf(xmf, "%s:/data\n", h5file_fullname.c_str());
+		fprintf(xmf, "        %s:/data\n", h5file_fullname.c_str());
 		fprintf(xmf, "       </DataItem>\n");
 		fprintf(xmf, "     </Attribute>\n");
 
---
+++ CubismApps/tools/vp2hdf/vpconvert.sh
@@ -0,0 +1,54 @@
+#MYSRCDIR=/tmp/CUBISM-DATA
+#MYDSTDIR=/tmp/CUBISM-DATA/VPCACHE
+QUALITYCHECK=0
+
+MYSRCDIR=.
+
+#get the number of threads
+{
+	printf "inquiring number of cores..."
+	
+	if [ $(uname) == "Darwin" ]
+	then
+		THREADS=$(sysctl hw.ncpu | cut -d" " -f2) 
+	else
+		{ THREADS=$(egrep -c "core id" /proc/cpuinfo 2> /dev/null) ; }  || \
+		{ printf "not able to recover number of cores\n" ; THREADS=1; }
+	fi
+	
+	printf "setting it to %d.\n" $THREADS
+}
+
+function generatechannel()
+{
+	(( $# == 3 )) || { printf "I was expecting 3 arguments, but i received %b\n" "$#"  ;  exit 2; }
+	
+	local MYCHANNEL=$1
+	local MYMAX=$2
+	local MYMIN=$3
+	
+	for F in ${MYSRCDIR}/*channel${MYCHANNEL} 
+	do
+		#ok, lets do it!
+		mpirun -mca btl sm,self -n $THREADS ./vp2hdf -simdata $F 
+		
+	done
+}
+
+T_CHECK_MSEC=0
+TSTART="$(date +%s)"
+
+generatechannel 0 0.6 1e3
+
+#it does not make sense to process velocity, so i skip this
+#generatechannel 1 -0.1 0.1
+#generatechannel 2 -0.1 0.1
+#generatechannel 3 -0.1 0.1
+
+generatechannel 4 0.02 100.0
+generatechannel 5 0.17 2.5; 
+generatechannel 6 3.47 4773.44
+
+TOTALTIME="$(($(date +%s)-TSTART))"
+T_CHECK=0
+echo Total time: $TOTALTIME seconds. Time spent in quality checks: $T_CHECK seconds. Ciao!
